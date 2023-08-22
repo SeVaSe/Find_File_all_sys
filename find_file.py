5e28fa6faed5dbaa4fbc05a4f6a9edda9a4b8414
sevase <kotaseva8@gmail.com>
2023-08-22 16:12:12 +0300

    поиск файла по системе и после поиск в нем нужных слов

--- find_file.py
+++ find_file.py
@@ -5,17 +5,12 @@ import os
 async def search_word(filename, keyword):
     with open(filename, 'r', encoding='utf-8') as fl:
         content = fl.read()
-        print('я тут')
 
         found_word = [wr for wr in keyword if wr in content]
-        print(f'В файле {os.path.basename(filename)} нашел такие слова из вашего списка: {found_word}')
+        print(f'В файле {os.path.basename(filename)} нашел такие слова из вашего списка: {found_word}\n')
 
-async def main():
-    files_list = ['tes_ai_1.txt', 'tes_ai_2.txt', 'план работы.txt']
-    keyword = ['Привет', 'васапа', 'таск1', 'таск2']
 
-    tasks = []
-
+async def find_file(files_list, keyword):
     while True:
         for root, dirs, files in os.walk('C:\\'):
             for file in files:
@@ -23,12 +18,26 @@ async def main():
                 if file in files_list:
                     print(f'Нашел файл: {file}')
                     path = os.path.join(root, file)
-
-                    task1 = asyncio.create_task(search_word(path, keyword))
-                    tasks.append(task1)
+                    await search_word(path, keyword)
 
 
-        await asyncio.gather(*tasks)
+
+
+
+async def main():
+    files_list = ['tes_ai_1.txt', 'tes_ai_2.txt', 'план работы.txt']
+    keyword = ['Привет', 'васапа', 'таск1', 'таск2']
+
+    tasks = []
+
+    tasks.append(find_file(files_list, keyword))
+
+
+
+
+
+
+    await asyncio.gather(*tasks)
 
 
 asyncio.run(main())
