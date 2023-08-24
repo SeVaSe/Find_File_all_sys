f049e04ead375ae41dad65118c2941d01e0f30da
sevase <kotaseva8@gmail.com>
2023-08-24 13:38:53 +0300

    улучшена структура + сделан выбор поиска по системе + сделана обработка текста, что улучшает пользовательский опыт

--- find_file.py
+++ find_file.py
@@ -2,42 +2,90 @@ import asyncio
 import os
 
 
-async def search_word(filename, keyword):
-    with open(filename, 'r', encoding='utf-8') as fl:
-        content = fl.read()
+class SearchFile:
+    """ПАНЕЛЬ УПРАВЛЕНИЯ"""
+    def __init__(self, files_list, keyword, function='да'):
+        self.files_list = files_list
+        self.keyword = keyword
+        self.function = function
 
-        found_word = [wr for wr in keyword if wr in content]
-        print(f'В файле {os.path.basename(filename)} нашел такие слова из вашего списка: {found_word}\n')
+    async def search_word(self, filename, keyword):
+        with open(filename, 'r', encoding='utf-8') as fl:
+            content = fl.read()
 
+            found_word = [wr for wr in keyword if wr in content]
+            print(f'В файле {os.path.basename(filename)} нашел такие слова из вашего списка: {found_word}\n')
 
-async def find_file(files_list, keyword):
-    while True:
+    async def find_file(self):
         for root, dirs, files in os.walk('C:\\'):
             for file in files:
-
-                if file in files_list:
-                    print(f'Нашел файл: {file}')
-                    path = os.path.join(root, file)
-                    await search_word(path, keyword)
-
-
-
-
-
-async def main():
-    files_list = ['tes_ai_1.txt', 'tes_ai_2.txt', 'план работы.txt']
-    keyword = ['Привет', 'васапа', 'таск1', 'таск2']
-
-    tasks = []
-
-    tasks.append(find_file(files_list, keyword))
+                #print(file)
+                match self.function:
+                    case 'да':
+                        if file.lower() in self.files_list:
+                            print(f'Нашел файл: {file}')
+                            path = os.path.join(root, file)
+                            await self.search_word(path, self.keyword)
+                    case 'нет':
+                        pass
 
 
+class PanelControl:
+    """ПАНЕЛЬ УПРАВЛЕНИЯ"""
+    def __init__(self, fileListName=None, fileListText=None):
+        self.fileListName = fileListName
+        self.fileListText = fileListText
+
+    async def findFileText(self, fileLName, fileTList):
+        files_list = fileLName         #tes_ai_1.txt, tes_ai_2.txt, план работы.txt
+        keyword = fileTList          #Привет, васапа, таск1, таск2
+        tasks = []
+
+
+        searchFile = SearchFile(files_list, keyword)
+        tasks.append(searchFile.find_file())
+
+        await asyncio.gather(*tasks)
+
+    async def findOsText(self, fileLName, fileTList, function):
+        print('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
+
+    async def input_name_file(self, function='да'):
+        match function:
+            case 'да':
+                await self.findFileText(self.fileListName, self.fileListText)
+            case 'нет':
+                await self.findOsText(self.fileListName, self.fileListText, function)
 
 
 
 
-    await asyncio.gather(*tasks)
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    while True:
+        a = input('да или нет: ')
+        match a.lower():
+            case 'да':
+                inNameFile = input('Введите имя/ена файла/ов через запятую: ').lower()
+                inTextFile = input('Введите слова, чтобы увидеть какой из данных файлов содержит данные слова: ').lower()
+                print('\n\n')
+
+                lNameFile = inNameFile.split(', ')
+                lTextFile = inTextFile.split(' ')
+                print(lNameFile, lTextFile)
+
+                pContrl = PanelControl(lNameFile, lTextFile)
+                asyncio.run(pContrl.input_name_file())
+
+            case 'нет':
+                inTextFile = input('Введите слова, чтобы увидеть список всех файлов, которые содержат данные слова: ')
+                print('\n\n')
+
+                lTextFile = inTextFile.split(' ')
+                print(lTextFile)
+
+                pContrl = PanelControl(lTextFile)
+                asyncio.run(pContrl.input_name_file(function='нет'))
+
+
